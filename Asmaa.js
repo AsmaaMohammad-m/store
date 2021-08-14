380ea15f18f0c829f1f26154b86232774bb2eb3b
AsmaaMohammad-m <88927718+AsmaaMohammad-m@users.noreply.github.com>
2021-08-14 12:11:56 +0200

    Add files via upload

---
+++ Asmaa.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$(".neqabdiv").animate(({transform  : "rotate(25deg)"}),1000);
+	  $(".div2").animate(({width  : "show"}),3000);
+	 
+	  
+	  $(".img").animate(({opacity:1}),3000);
+	   $(".img").hover(function(){
+    $(".img").css("border","3px solid rgb(12, 145, 197)");
+    $(".img").css("border","3px solid rgb(12, 145, 197)");
+      
+//     
+}
+  
+)
+}
+
+
+
+)
